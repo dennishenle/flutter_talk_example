3cb7fc5031b65c00c01e10025fb1a74462ccabbe
dennishenle <henledennis@gmail.com>
2025-01-29 08:32:10 +0100

    feat: finished ui

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home', style: TextStyle(fontSize: 24)),
+    return BlocProvider(
+      create: (context) => HomeCubit(),
+      child: BlocBuilder<HomeCubit, int>(
+        builder: (ctx, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Home'),
+            ),
+            body: Center(
+              child: Text('$state', style: TextStyle(fontSize: 32)),
+            ),
+          );
+        },
+      ),
     );
   }
+}
+
+class HomeCubit extends Cubit<int> {
+  HomeCubit() : super(0);
+
+  void increment() => emit(state + 1);
 }
--- lib/set_screen.dart
+++ lib/set_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SetScreen extends StatelessWidget {
   const SetScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Increment', style: TextStyle(fontSize: 24)),
+    return BlocProvider(
+      create: (context) => SetCubit(),
+      child: BlocBuilder<SetCubit, int>(
+        builder: (ctx, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Set'),
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('$state'),
+              ),
+            ),
+            body: Center(
+              child: MaterialButton(
+                color: Colors.amber,
+                onPressed: ctx.read<SetCubit>().increment,
+                child: const Text('Increment',
+                    style: TextStyle(
+                      fontSize: 32,
+                    )),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
+}
+
+class SetCubit extends Cubit<int> {
+  SetCubit() : super(0);
+
+  void increment() => emit(state + 1);
 }
